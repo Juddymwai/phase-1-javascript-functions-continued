1883502ddc629c56ba0d00ddfae37e5042b4ed3f
Judy Mwai <juddiemukami@gmail.com>
2022-06-08 00:03:35 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+function saturdayFun (string ='roller-skate'){
+    return `This Saturday, I want to ${string}!`
+}
+console.log (saturdayFun ('bathe my dog'));
+
+let mondayWork = function (string = "go to the office"){
+    return `This Monday, I will ${string}.`
+
+}
+ console.log (mondayWork('work from home'));
+
+ function wrapAdjective(){
+     
+     return function (innerpara="special"){
+         return `You are ${innerpara}`
+
+     };
+ }
